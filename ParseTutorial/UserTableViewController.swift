a53788d6cc3ccd6c4391aefe06093ab5309a816b
RafaelGarciaIII <garcia.rafael.iii@gmail.com>
2015-11-15 14:43:13 -0700

    Adds buttons to user prefs view

---
+++ ParseTutorial/UserTableViewController.swift
@@ -0,0 +1,41 @@
+//
+//  UserTableViewController.swift
+//  ParseTutorial
+//
+//  Created by Rafael Garcia on 11/14/15.
+//  Copyright © 2015 bizzi-body. All rights reserved.
+//
+
+import UIKit
+
+class UserTableViewController: PFQueryTableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    /*
+    @IBAction func add(sender: AnyObject) {
+        
+        dispatch_async(dispatch_get_main_queue()) {
+            self.performSegueWithIdentifier("TableViewToDetailBrewView", sender: self)
+        }
+    }*/
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
